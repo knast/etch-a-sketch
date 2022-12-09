2050cc403d334c5706b35007093d60617a03e396
knast <knast808@gmail.com>
2022-12-09 18:07:29 +0100

    Add buttons for adding a new grid and restarting

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ const gridContainer = document.querySelector('.container');
 const divInContainer = document.createElement('div');
 divInContainer.style.display = 'inline';
 
-for(let i = 0; i < 256; i++) {
-    const divInContainer = document.createElement('div');
-    gridContainer.appendChild(divInContainer);
-    divInContainer.addEventListener('mousedown', () => {
-        divInContainer.style.backgroundColor = '#24C8FA';
+const newGridBtn = document.querySelector('.new-grid');
+const restartBtn = document.querySelector('.restart');
+
+
+
+newGridBtn.addEventListener('click', () => {
+    let userInputWidth = Number(prompt('How many blocks in width?'));
+    let userInputHeight = Number(prompt('How many blocks in height?'));
+    let gridColor = ((prompt('What color of the grid do you want?')).toString()).toLowerCase();
+    gridContainer.style.gridTemplateColumns = `repeat(${userInputHeight}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${userInputWidth}, 1fr)`;
+    for(let i = 0; i < userInputWidth*userInputHeight; i++) {
+        const divInContainer = document.createElement('div');
+        divInContainer.style.borderRadius = '8px';
+        divInContainer.classList.add('in-container');
+        gridContainer.appendChild(divInContainer);
+        divInContainer.addEventListener('mouseover', () => {
+            divInContainer.style.backgroundColor = gridColor;
+        })
+    }
+    restartBtn.addEventListener('click', () => {
+        for(let j = 0; j < userInputWidth*userInputHeight; j++) {
+            const partOfGrid = document.querySelector('.in-container');
+            gridContainer.removeChild(partOfGrid);
+        }
     })
-   
-    
-}
+})
+
 
 
 
